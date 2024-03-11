cb36a3e89690bcd174292a71a99decb3990af273
MisanthropicBit <albo.developer@gmail.com>
2024-03-11 22:33:29 +0100

    Working async test

--- tests/async_test_spec.lua
+++ tests/async_test_spec.lua
@@ -1,17 +1,31 @@
 local async = require("neotest-busted.async")
+local control = require("neotest.async").control
 
 describe("async tests", function()
     before_each(async(function()
-        vim.wait(2000, function()
-            vim.print("before_each")
+        vim.wait(200, function()
             return false
-        end, 500)
+        end, 50)
     end))
 
     it("async test 1", async(function()
-        vim.wait(2000, function()
-            vim.print("hello")
+        vim.wait(100, function()
             return false
         end, 500)
     end, 40))
+
+    it("async test 2", async(function()
+        local timer = vim.loop.new_timer()
+        local event = control.event()
+
+        -- Print a message after 1 second
+        timer:start(1000, 0, function()
+            timer:stop()
+            timer:close()
+            vim.print("Hello from async test")
+            event.set()
+        end)
+
+        event.wait()
+    end))
 end)
