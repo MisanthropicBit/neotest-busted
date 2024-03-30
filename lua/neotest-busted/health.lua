c5b0a2e7a18821739d90af043bd30822ec30291f
MisanthropicBit <albo.developer@gmail.com>
2024-03-30 22:06:25 +0100

    Nio required, inverse condition

--- lua/neotest-busted/health.lua
+++ lua/neotest-busted/health.lua
@@ -6,15 +6,13 @@ local config = require("neotest-busted.config")
 local min_neovim_version = "0.9.0"
 
 ---@param module_name string
----@param not_installed_reporter fun(msg: string)?
-local function check_module_installed(module_name, not_installed_reporter)
+local function check_module_installed(module_name)
     local installed, _ = pcall(require, module_name)
 
     if installed then
         vim.health.report_ok(("`%s` is installed"):format(module_name))
     else
-        local _not_installed_reporter = not_installed_reporter or vim.health.report_ok
-        _not_installed_reporter(("`%s` is not installed"):format(module_name))
+        vim.health.report_error(("`%s` is not installed"):format(module_name))
     end
 end
 
@@ -30,7 +28,7 @@ function health.check()
     -- NOTE: We cannot check the neotest version because it isn't avertised as
     -- part of its public api
     check_module_installed("neotest")
-    check_module_installed("nio", vim.health.report_warn)
+    check_module_installed("nio")
 
     local ok, error = config.validate(config)
 
@@ -42,17 +40,17 @@ function health.check()
 
     local busted = adapter.find_busted_command()
 
-    if not busted then
-        vim.health.report_error(
-            "could not find busted executable",
-            "please install busted using luarocks (https://luarocks.org/)"
-        )
-    else
+    if busted then
         vim.health.report_ok(
             ("found `busted` (type: %s) at\n%s"):format(
                 busted.type,
                 vim.loop.fs_realpath(busted.command)
             )
+        )
+    else
+        vim.health.report_error(
+            "could not find busted executable",
+            "please install busted using luarocks (https://luarocks.org/)"
         )
     end
 end
